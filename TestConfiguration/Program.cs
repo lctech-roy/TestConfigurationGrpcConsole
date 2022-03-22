86107490cab7797f3622af9e28f69dc65cb5eba1
lctech-roy <Roy@lctech.com.tw>
2022-03-22 23:01:02 +0800

    First

---
+++ TestConfiguration/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using Lctech.Configuration.Protobufs.HelloWorld;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:8080");
+var client = new HelloWorldService.HelloWorldServiceClient(channel);
+var reply = await client.GetWorldAsync(
+    new GetWorldRequest() { World = "Hello" });
+Console.WriteLine("Greeting: " + reply.World);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
