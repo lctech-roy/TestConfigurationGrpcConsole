adc7594b4459412b925abe812e814ae1262546af
lctech-roy <Roy@lctech.com.tw>
2022-03-23 15:27:33 +0800

    Change to Configuration

--- TestConfiguration/Program.cs
+++ TestConfiguration/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Grpc.Net.Client;
-using Lctech.Configuration.Protobufs.HelloWorld;
+using Lctech.Configuration.Protobufs.Configuration;
 
 using var channel = GrpcChannel.ForAddress("http://localhost:8080");
-var client = new HelloWorldService.HelloWorldServiceClient(channel);
-var reply = await client.GetWorldAsync(
-    new GetWorldRequest() { World = "Hello" });
+var client = new ConfigurationService.ConfigurationServiceClient(channel);
+var reply = await client.GetConfigurationAsync(
+    new GetConfigurationRequest() { World = "Hello" });
 Console.WriteLine("Greeting: " + reply.World);
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
