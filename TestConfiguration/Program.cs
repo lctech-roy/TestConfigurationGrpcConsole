f48e94e7a03e855c239339311fa7f67a1a16730e
lctech-roy <Roy@lctech.com.tw>
2022-03-25 23:01:16 +0800

    Add test insert & update service

--- TestConfiguration/Program.cs
+++ TestConfiguration/Program.cs
@@ -6,18 +6,63 @@ using ProtobufsConfiguration = Lctech.Configuration.Protobufs.Configuration.Conf
 
 using var channel = GrpcChannel.ForAddress("http://localhost:8080");
 var client = new ConfigurationService.ConfigurationServiceClient(channel);
-var reply = await client.GetConfigurationsByGroupAsync(
-    new GetConfigurationRequest() { Group = "common", Level = 1});
 
-var config = new TypeAdapterConfig();
+//查詢
 
-config.NewConfig<ProtobufsConfiguration, EntitiesConfiguration>()
-    .Map(dest => dest.CreationDate, src => src.CreationDate.ToDateTimeOffset())
-    .Map(dest => dest.ModificationDate, src => src.ModificationDate.ToDateTimeOffset())
-    .Compile();
+// var reply = await client.GetConfigurationsAsync(
+//     new GetConfigurationRequest() { Group = "emoji", Level = 2});
+//
+// var config = new TypeAdapterConfig();
+//
+// config.NewConfig<ProtobufsConfiguration, EntitiesConfiguration>()
+//     .Map(dest => dest.CreationDate, src => src.CreationDate.ToDateTimeOffset())
+//     .Map(dest => dest.ModificationDate, src => src.ModificationDate.ToDateTimeOffset())
+//     .Compile();
 
-var configurations = reply.Configurations.Adapt<List<EntitiesConfiguration>>(config);
+// var configurations = reply.Configurations.Adapt<List<EntitiesConfiguration>>(config);
 
-Console.WriteLine("Greeting: " + reply.Configurations);
+//新增
+
+// var insertConfigurations = new List<InsertConfiguration>();
+// insertConfigurations.Add(new InsertConfiguration()
+// {
+//     Group = "testGroup",
+//     Source = "testSource",
+//     Value = "testValue_3",
+//     ParentId = 180820643338518528
+// });
+// insertConfigurations.Add(new InsertConfiguration()
+// {
+//     Group = "testGroup",
+//     Source = "testSource",
+//     Value = "testValue_4",
+//     ParentId = 180820643338518528
+// });
+//
+// var reply = await client.InsertConfigurationsAsync(
+//     new InsertConfigurationRequest() { Configurations = { insertConfigurations }});
+
+//修改
+var updateConfigurations = new List<UpdateConfiguration>();
+updateConfigurations.Add(new UpdateConfiguration()
+{
+    Id = 180848843905040384,
+    Value = "testValue_4_update",
+    SortingIndex = 4,
+    ModifierId = 4,
+});
+updateConfigurations.Add(new UpdateConfiguration()
+{
+    Key = "test_5",
+    Value = "testValue_5_update",
+    SortingIndex = 5,
+    ModifierId = 5,
+});
+
+var reply = await client.UpdateConfigurationsAsync(
+    new UpdateConfigurationRequest() { Configurations = { updateConfigurations }});
+
+
+Console.WriteLine("Greeting: " );
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
